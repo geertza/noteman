25d1111e1b185e30c8ce67740e8b10549de90f0c
geertza <geertza@gmail.com>
2020-05-15 02:27:59 -0600

    final

--- noteserver.js
+++ noteserver.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var fs = require("fs");
-var path = require("path");
 var note = require("./db/db.json");
 
 var app = express();
@@ -15,36 +14,47 @@ app.use(express.json());
 // savenotes 
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
+    // create id parameter
+    newNote.id= newNote.title;
     note.push(newNote);
     let saveToJSON = JSON.stringify(note);
-    fs.writeFile("./db/db.json", saveToJSON, function (err) {
-        if (err) return console.log(err);
-        console.log('fs write err=   ----'+err);
-      });
+    fs.writeFile("./db/db.json", saveToJSON, err => { if (err) throw err });
+        
     res.redirect('back');
    
 } )
 
-// display notes
+// display note list and notes 
 app.get("/api/notes", (req, res) => {
     return res.json(note);
 });
 
-
+// server listen
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
 
+// delete notes
+app.delete("/api/notes/:id", (req, res) => {
+    const deleted = note.findIndex((i) => i.id == req.params.id);
+    note.splice(deleted, 1);
+    let saveToJSON=JSON.stringify(note);
+    fs.writeFile("./db/db.json", saveToJSON, err => { if (err) throw err });
+    res.json(note);
+    res.redirect('back');
 
+});
 // deletes and resets to default must be entered in http bar
   app.get("/clear", function(req, res) {
     // Empty out the array of data
    
-    let saveToJSON=JSON.stringify([{"title":"Test Title","text":"Test text"}]);
-   fs.writeFile("./db/db.json", saveToJSON, function (err) {
-    if (err) return console.log(err);
-    console.log('clear fs write err'+err);
-  });
-    console.log(saveToJSON);
+    let saveToJSON=JSON.stringify([]);
+    fs.writeFile("./db/db.json", saveToJSON, err => { if (err) throw err });
     res.redirect('back');
   });
+
+
+
+
+
+  
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Noteman
+  BY Andy Geertz       ==npm:express== ==fs== 
+   
+  
+  <img src= ./sampleImage.png></img>
+  
+  ## Table of Contents
+  * [Purpose And Description](#purpose-and-description-of-application)
+  * [Installation](#installation-or-deployed-website)
+  * [instructions](#Instructions-for-use) 
+  * [Future Plans](#future-development)
+  * [contact](#contact-info)
+  
+  ## Purpose And Description Of Application
+  For users that need to keep track of a lot of information, it's easy to forget or be unable to recall something important. Being able to take persistent notes allows users to have written information available when needed.
+Created a server express based webpage to create, store, delete, notes.
+  
+  ## Installation Or Deployed Website
+* download root folder
+* install npm modules
+* load node noteserver
+  
+  ## Instructions For Use
+click go to notes, type in new note, hit svae icon to save. click on note to read. click on delete icon to delete. 
+  
+
+  
+
+  
+  ## Future Development
+  Would add folders to better organize notes..
+
+  ## Contact Info
+          Github id    geertza
+          Email        null
+          If You have any questions fill free to respond to my Github account.
+![profile image](https://avatars3.githubusercontent.com/u/60946979?v=4) 
